752dea64a3c7c1106674c72f86072fcaa779abfe
MarcinDudek <dudek.v@gmail.com>
2024-09-19 12:16:07 +0200

    feat: pint in hex, capture newline and chanche to dot

---
+++ src/011-xxd_clone.zig
@@ -0,0 +1,34 @@
+// xxd clone
+// TODO: change debug to stdout
+// TODO: print bytes in hex format
+// TODO: capture newline and change it into dot .
+
+const std = @import("std");
+const print = std.debug.print;
+
+pub fn main() !void {
+    const stdin = std.io.getStdIn().reader();
+    const stdout = std.io.getStdOut().writer();
+
+    // var buffer: [4]u8 = undefined;
+    var buffer: [1024]u8 = undefined;
+    var c: u8 = undefined;
+
+    while (true) {
+        const bytes_read = try stdin.read(&buffer);
+        if (bytes_read == 0) {
+            break;
+        }
+        for (buffer[0..bytes_read], 0..) |byte, index| {
+            c = byte;
+            if (byte == '\n') {
+                c = '.';
+                buffer[index] = c;
+            }
+            try stdout.print("{X:0>2} ", .{c});
+        }
+        try stdout.print("{s}", .{[_]u8{' '} ** 20});
+        try stdout.print("{s}\n", .{buffer[0..bytes_read]});
+    }
+    std.debug.print("Got EOF.", .{});
+}
