58d3db10b4ad4c18b59f8324fd6749c9c67025c4
MarcinDudek <dudek.v@gmail.com>
2024-09-20 12:03:56 +0200

    bug

--- mini-projects/xxd-clone/004-xxd_clone.zig
+++ mini-projects/xxd-clone/004-xxd_clone.zig
@@ -1,6 +1,8 @@
 // xxd clone
 // DONE: add colors into string formating, using ANSI escape codes
 // DONE: check if given char is ASCII or UTF-8, added isUTF_8 fn
+// TODO: print string char by char checking if UTF-8 if yes than print in color
+// BUG: UFT-8 one char can have more than one byte !!! SOLUTION in next iteration
 
 const std = @import("std");
 const print = std.debug.print;
@@ -22,6 +24,7 @@ pub fn main() !void {
             break;
         }
         for (buffer[0..bytes_read], 0..) |byte, index| {
+            // check for line-feed
             if (byte == '\n') {
                 buffer[index] = '.';
             }
@@ -32,9 +35,20 @@ pub fn main() !void {
                 try stdout.print("{X:0>2} ", .{byte});
             }
         }
+        // spacing
         try stdout.print("{s}", .{[_]u8{' '} ** 20});
-        // print char in collor
-        try stdout.print("{s}{s}{s}\n", .{ green, buffer[0..bytes_read], reset });
+
+        // print finall string char by char, if UTF-8 than in collor
+        for (buffer[0..bytes_read]) |byte| {
+            if (isUFT_8(byte)) {
+                try stdout.print("{s}{s}{s}", .{ green, [1]u8{byte}, reset });
+            } else {
+                try stdout.print("{s}", .{[1]u8{byte}});
+            }
+        }
+        // need new line bc. we swap all \n into dot .
+        try stdout.print("\n", .{});
+        // try stdout.print("{s}{s}{s}\n", .{ green, buffer[0..bytes_read], reset });
     }
     std.debug.print("Got EOF.", .{});
 }
