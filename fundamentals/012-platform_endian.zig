71a87e04e0674e7ca7a301741cc04062911cc4d1
MarcinDudek <dudek.v@gmail.com>
2024-09-22 18:51:42 +0200

    how to get platform endian platform type

---
+++ fundamentals/012-platform_endian.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+const native_endian = @import("builtin").target.cpu.arch.endian();
+
+pub fn main() void {
+    switch (native_endian) {
+        .big => {
+            std.debug.print("Little-endian\n", .{});
+        },
+        .little => {
+            std.debug.print("Big-endian\n", .{});
+        },
+    }
+}
