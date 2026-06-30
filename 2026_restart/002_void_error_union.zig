0a1b588f1726e5487127b44dc3096d8b8712434b
MarcinDudek <dudek.v@gmail.com>
2026-06-30 12:55:27 +0200

    add function call to c() in main for demonstration

--- 2026_restart/002_void_error_union.zig
+++ 2026_restart/002_void_error_union.zig
@@ -7,8 +7,15 @@ pub fn main(init: std.process.Init) !void {
     stdout.writeStreamingAll(init.io, "Hello, World!\n") catch {};
 
     b();
+    c();
 }
 
 fn b() void {
     std.debug.print("b() called\n", .{});
+}
+
+fn c() void {
+    const print = std.debug.print;
+    print("c() called\n", .{});
+    // do not need to try or catch here because print() cannot fail -> returns void
 }
